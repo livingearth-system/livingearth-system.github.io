f44cc574bda6ae360bffd8fcc2d287f957af63ac
Sergey Miron <sergey.miron@icloud.com>
2022-11-02 17:25:30 +0200

    initial

---
+++ index.php
@@ -0,0 +1,394 @@
+<style>
+button a:hover,
+button a {
+    color: #000;
+    text-decoration: none;
+}
+.pt-2 a {
+    color: #1D2865 !important;
+}
+</style>
+<?php  $activepage="home"; include 'header.php'; ?>
+    <!-- welcome-to-start -->
+	<div class="main-home-section">
+		<img class="main-benner-img" src="assets/img/home.jpg" alt="">
+		<div class="container welcome-living-main">
+			<div class="row">
+				<div class="col-12 col-lg-8 welcome-living-earth">
+					<h5>all-in-one</h5>
+					<h1>Welcome to Living Earth</h1>
+					<p>Living Earth is providing new capacity to support planetary restoration, primarily through use of Earth observations.</p>
+					<div class="welcome-living-button">
+						<button type="button" class="search hover-slide-right">Search</button>
+						<button type="button" onclick="location.href='/contact'" class="get-in-touch hover-slide-transparent">Get in touch</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- welcome-to-end -->
+
+	<!-- search-landscapes-start -->
+	<div class="container mt-80 search-landscapes-main">
+		<div class="row">
+			<div class="col-12 col-lg-8 m-auto">
+				<div class="landscapes-title">search for landscapes</div>
+				<div class="landscapes-search-bar">
+					<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M19.0488 18.0609C21.7308 14.8557 21.5685 10.0709 18.5622 7.05683C15.3834 3.86989 10.2292 3.86989 7.05036 7.05683C3.87254 10.2428 3.87254 15.4074 7.05036 18.5933C10.0743 21.6243 14.8858 21.772 18.0839 19.0364L22.1615 23.1309C22.4289 23.3994 22.8633 23.4003 23.1317 23.1329C23.4002 22.8656 23.4011 22.4312 23.1337 22.1627L19.0488 18.0609ZM17.5908 8.02579C14.9481 5.37633 10.6645 5.37633 8.0218 8.02579C5.37814 10.6762 5.37814 14.9739 8.02174 17.6243C10.6644 20.2732 14.9481 20.2732 17.5908 17.6244C20.2344 14.9739 20.2344 10.6762 17.5908 8.02579Z" fill="#605C72"/>
+					</svg>
+					<input type="text" placeholder="Search by area, date, tools">
+					<button type="button">
+						<span>Search</span>
+						<svg width="28" height="28" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" clip-rule="evenodd" d="M19.0488 18.0609C21.7308 14.8557 21.5685 10.0709 18.5622 7.05683C15.3834 3.86989 10.2292 3.86989 7.05036 7.05683C3.87254 10.2428 3.87254 15.4074 7.05036 18.5933C10.0743 21.6243 14.8858 21.772 18.0839 19.0364L22.1615 23.1309C22.4289 23.3994 22.8633 23.4003 23.1317 23.1329C23.4002 22.8656 23.4011 22.4312 23.1337 22.1627L19.0488 18.0609ZM17.5908 8.02579C14.9481 5.37633 10.6645 5.37633 8.0218 8.02579C5.37814 10.6762 5.37814 14.9739 8.02174 17.6243C10.6644 20.2732 14.9481 20.2732 17.5908 17.6244C20.2344 14.9739 20.2344 10.6762 17.5908 8.02579Z" />
+						</svg>
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-6 col-md-3 col-lg-2 landscapes-catogories">
+				<a href="javascript:void(0)">
+					<img src="assets/img/Africa.jpg" alt="img">
+					<span>Africa</span>
+				</a>
+			</div>
+			<div class="col-6 col-md-3 col-lg-2 landscapes-catogories">
+				<a href="javascript:void(0)">
+					<img src="assets/img/americas.jpg" alt="img">
+					<span>americas</span>
+				</a>
+			</div>
+			<div class="col-6 col-md-3 col-lg-2 landscapes-catogories">
+				<a href="javascript:void(0)">
+					<img src="assets/img/Asia.jpg" alt="img">
+					<span>Asia</span>
+				</a>
+			</div>
+			<div class="col-6 col-md-3 col-lg-2 landscapes-catogories">
+				<a href="/europe">
+					<img src="assets/img/Europe.jpg" alt="img">
+					<span class="mb-0">Europe</span>
+				</a>
+			</div>
+			<div class="col-6 col-md-3 col-lg-2 landscapes-catogories">
+				<a href="oceania">
+					<img src="assets/img/OceanIa.jpg" alt="img">
+					<span class="mb-0">OceanIa</span>
+				</a>
+			</div>
+		</div>
+	</div>
+	<!-- search-landscapes-end -->
+
+	<!-- future-landscapes-start -->
+	<div class="container mt-80 future-landscapes-main">
+		<h2 class="common-title">Future landscapes</h2>
+		<div class="row">
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/future-landscapes/reimagining/"><img src="assets/img/Future1.jpg" alt="img"></a>
+				<div class="future-dsc">
+					<a href="/future-landscapes/reimagining/"><div class="future-dsc-title">(Re)imagining</div></a>
+					<p>What would you like to see?</p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/future-landscapes/harmonising/"><img src="assets/img/Future2.jpg" alt="img"></a>
+				<div class="future-dsc">
+					<a href="/future-landscapes/harmonising/"><div class="future-dsc-title">Harmonising</div></a>
+					<p>How can we agree?</p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/future-landscapes/realising/"><img src="assets/img/Future3.jpg" alt="img"></a>
+				<div class="future-dsc">
+					<a href="/future-landscapes/realising/"><div class="future-dsc-title">Realising</div></a>
+					<p>How can we be certain?</p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/future-landscapes/facilitating/"><img src="assets/img/Future4.jpg" alt="img"></a>
+				<div class="future-dsc">
+					<a href="/future-landscapes/facilitating/"><div class="future-dsc-title">Facilitating</div></a>
+					<p>How can we track progress?</p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/future-landscapes/valuing/"><img src="assets/img/Future5.jpg" alt="img"></a>
+				<div class="future-dsc">
+					<a href="/future-landscapes/valuing/"><div class="future-dsc-title">Valuing</div></a>
+					<p>How can we value our futures?</p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/future-landscapes/assessing-risk/"><img src="assets/img/Future6.jpg" alt="img"></a>
+				<div class="future-dsc">
+					<a href="/future-landscapes/assessing-risk/"><div class="future-dsc-title">Assessing Risk</div></a>
+					<p>How can we protect our assets?</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- future-landscapes-end -->
+
+
+	<!-- aboutus-start -->
+	<div class="container mt-80 aboutus-main">
+		<div class="row">
+			<div class="col-12 col-md-6 aboutus-left">
+				<div class="aboutus-left-dsc">
+					<h5>about us</h5>
+					<h2>we are driven by the same purpose</h2>
+					<p>Across the world, there is a desire to protect our planet for current and future generations but achieving this often seems beyond reach.   Living Earth is therefore providing one avenue by which people can work openly and together to understand and plan our future landscapes so that they benefit both ourselves but also nature.</p>
+					<button type="button">Learn more</button>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 aboutus-right-img">
+				<img src="assets/img/pexels-anne.jpg" alt="img">
+			</div>
+		</div>
+	</div>
+	<!-- aboutus-start -->
+
+	<!-- Past & Current landscapes-start -->
+	<div class="container mt-100 pastcurrent-landscapes-main">
+		<h2 class="common-title">Past &amp; Current Landscapes <a href="/past-present-landscapes/">View all</a></h2>
+		<p>From knowledge of the past, we can inform the future but we need the tools to capture how we have historically shaped the landscapes of today.   Earth observation data from satellites and aircraft give us a unique insight and so we have worked to develop a series of globally-applicable tools that allow us to describe, map and monitor land covers, habitats and ecosystems.</p>
+		<div class="row effect">
+			<div class="col-12">
+				<div id="owl-past-current" class="owl-carousel owl-theme owl-img-responsive">
+					<div class="item">
+						<a href="#"><img src="/assets/img/PastCurrent1.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="#">Analysis Ready Data</a></h4>
+							<p>Download and pre-process satellite sensor data to an analysis ready format</p>
+						</div>
+					</div>
+					<div class="item">
+						<a href="/data/environmental-descriptors"><img src="/assets/img/PastCurrent2.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="/data/environmental-descriptors">Environmental Descriptors</a></h4>
+							<p>Obtain from existing sources oor retrieve or classify from Earth observation data</p>
+						</div>
+					</div>
+					<div class="item">
+						<a href="#"><img src="/assets/img/PastCurrent3.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="#">Mapping</a></h4>
+							<p>Combine the environmental descriptors to generate maps of land cover</p>
+						</div>
+					</div>
+					<div class="item">
+						<a href="#"><img src="/assets/img/PastCurrent4.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="#">Monitoring</a></h4>
+							<p>Consistenty describe and quantify change based on impacts and pressures</p>
+						</div>
+					</div>
+					<div class="item">
+						<a href="#"><img src="/assets/img/PastCurrent5.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="#">Validation and calibration</a></h4>
+							<p>Record environmental descriptors, land cover, habitats and change on the ground</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Past & Current landscapes-end -->
+
+	<!-- TOOLS landscapes-start -->
+	<div class="container mt-100 mb-100 tools-main">
+		<h2 class="common-title">TOOLS</h2>
+		<div class="row">
+			<div class="col-12">
+				<div id="owl-tools" class="owl-carousel owl-theme owl-img-responsive">
+					<div class="item">
+						<a href="#"><img src="assets/img/tools1.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="#">See on Map</a></h4>
+						</div>
+					</div>
+					<div class="item">
+						<a href="javascript:void(0)"><img src="assets/img/tools2.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="javascript:void(0)">Get the data</a></h4>
+						</div>
+					</div>
+					<div class="item">
+						<a href="https://jupyter.org" target="_blank"><img src="assets/img/tools3.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="https://jupyter.org" target="_blank">Analyse the data</a></h4>
+						</div>
+					</div>
+					<div class="item">
+						<a href="#"><img src="assets/img/tools4.jpg" alt="img" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="#">Models</a></h4>
+						</div>
+					</div>
+					<div class="item">
+						<a href="/bigdata/"><img src="assets/img/tools5.jpg" alt="Big data" /></a>
+						<div class="pastcurrent-dsc">
+							<h4><a href="/bigdata/">Big data</a></h4>
+						</div>
+					</div>
+				</div>
+            </div>
+        </div>
+	</div>
+	<!-- TOOLS landscapes-end -->
+
+	<!-- contribute-Start -->
+	<div class="container mb-100 contribute-main">
+		<div class="row">
+			<div class="col-12 col-md-6 contribute-left-img">
+				<img src="assets/img/contribute.jpg" alt="img"/>
+			</div>
+			<div class="col-12 col-md-6 contribute-right">
+				<div class="contribute-left-dsc">
+					<h5>join livingearth</h5>
+					<h2>How to contribute</h2>
+					<p>Living Earth is an open resource that allows individuals, groups, organisations and countries/regions to better understand how to plan the restoration of landscapes through knowledge of the past.   Living Earth also provides a structure that facilitates collaboration in the planning of landscapes and realisation of goals and ambitions.</p>
+					<button type="button">Learn more</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- contribute-End -->
+
+	<!-- stay-up-Start -->
+	<div class="container mb-100">
+		<div class="common-blog-main">
+			<p>our blog</p>
+			<div class="common-blog-title">
+				<h3>Stay up to date</h3>
+				<button type="button">Read all articles</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<div id="owl-stay-up" class="owl-carousel owl-theme owl-img-responsive">
+					<div class="item">
+		                <div class="stay-up-card">
+		                    <a href="javascript:void(0)"><img src="assets/img/forecasts.jpg" alt=""></a>
+		                    <button>forecasts</button>
+		                    <p><a href="javascript:void(0)">Helping to improve the retrieval of forest carbon stocks</a></p>
+		                </div>
+					</div>
+					<div class="item">
+		                <div class="stay-up-card">
+		                    <a href="javascript:void(0)"><img src="assets/img/oldman.jpg" alt=""></a>
+		                    <button>Research</button>
+		                    <p><a href="javascript:void(0)">Using tree species maps from Sentinel-2 and Planet data to inform on potential future impacts of plant disease.</a></p>
+		                </div>
+					</div>
+					<div class="item">
+		                <div class="stay-up-card">
+		                    <a href="javascript:void(0)"><img src="assets/img/forecasts.jpg" alt=""></a>
+		                    <button>forecasts</button>
+		                    <p><a href="javascript:void(0)">A new global change taxonomy</a></p>
+		                </div>
+					</div>
+				</div>
+	        </div>
+		</div>
+	</div>
+	<!-- stay-up-End -->
+
+	<!-- join-our-events-Start -->
+	<div class="container mb-100">
+		<div class="common-blog-main">
+			<p>our events</p>
+			<div class="common-blog-title">
+				<h3>join our events</h3>
+				<button type="button">Check All Events</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-6 join-our-events">
+	            <div class="join-our-card">
+	                <a href="javascript:void(0)"><img src="assets/img/eventImage.jpg" alt=""></a>
+	                <div class="join-our-dsc">
+		                <button>10th November, 2022</button>
+		                <p><a href="javascript:void(0)">Protecting our Planet Day (POP22)</a></p>
+	                </div>
+	            </div>
+			</div>
+			<div class="col-12 col-md-6 join-our-event-main">
+				<div class="row our-event-main">
+					<div class="col-3 pr-0 pr-lg-3">
+						<a href="javascipt:void(0)"><img class="event-our-img" alt="img" src="assets/img/our-event1.jpg" /></a>
+					</div>
+					<div class="col-9">
+						<div class="event-our-right-main">
+							<div class="event-our-right-inner">
+								<button>11th November, 2022</button>
+								<span>online</span>
+							</div>
+							<p><a href="javascipt:void(0)">How AI can improve the speed and accuracy of geological researches</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="row our-event-main">
+					<div class="col-3 pr-0 pr-lg-3">
+						<a href="javascipt:void(0)"><img class="event-our-img" alt="img" src="assets/img/our-event2.jpg" /></a>
+					</div>
+					<div class="col-9">
+						<div class="event-our-right-main">
+							<div class="event-our-right-inner">
+								<button>12th November, 2022</button>
+								<span>online</span>
+							</div>
+							<p><a href="javascipt:void(0)">How AI can improve the speed and accuracy of geological researches</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="row our-event-main">
+					<div class="col-3 pr-0 pr-lg-3">
+						<a href="javascipt:void(0)"><img class="event-our-img" alt="img" src="assets/img/our-event3.jpg" /></a>
+					</div>
+					<div class="col-9">
+						<div class="event-our-right-main">
+							<div class="event-our-right-inner">
+								<button>13th November, 2022</button>
+								<span>online</span>
+							</div>
+							<p><a href="javascipt:void(0)">How AI can improve the speed and accuracy of geological researches</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="row our-event-main">
+					<div class="col-3 pr-0 pr-lg-3">
+						<a href="javascipt:void(0)"><img class="event-our-img" alt="img" src="assets/img/our-event1.jpg" /></a>
+					</div>
+					<div class="col-9">
+						<div class="event-our-right-main">
+							<div class="event-our-right-inner">
+								<button>14th November, 2022</button>
+								<span>online</span>
+							</div>
+							<p><a href="javascipt:void(0)">How AI can improve the speed and accuracy of geological researches</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- join-our-events-End -->
+
+	<!-- get-in-section-Start -->
+	<div class="container mb-100">
+		<div class="get-in-section-main">
+			<div class="get-in-section-dsc">
+				<h3>WE NEED YOUR HELP...</h3>
+				<p>Find out how you contribute to the website by joining our community and helping the Living Earth Hub grow.</p>
+			</div>
+			<button type="button" onclick="location.href='/contact'">Get in touch</button>
+		</div>
+	</div>
+	<!-- get-in-section-End -->
+
+    <?php include 'footer.php'; ?>
