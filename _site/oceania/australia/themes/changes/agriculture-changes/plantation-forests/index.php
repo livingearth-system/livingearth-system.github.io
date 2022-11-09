1d7e5755c7c7364c0a7175458011e96321e3e36a
Sergey Miron <sergey.miron@icloud.com>
2022-11-09 13:59:58 +0200

    updating themes

---
+++ _site/oceania/australia/themes/changes/agriculture-changes/plantation-forests/index.php
@@ -0,0 +1,133 @@
+<?php
+// themes/changes/agriculture-changes/plantation-forests/
+$activepage=""; include($_SERVER['DOCUMENT_ROOT'].'/header.php'); ?>
+
+  <!--Faciliting-section-start -->
+  <div class="container">
+		<div class="row">
+			<div class="col-12 mt-60" >
+                <h5 class="common-title">Plantation forests</h5>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-ms-9 col-lg-9 col-xl-9 col-xxl-9">
+                <div class="pb-5">
+                	<div class="bg-dark pb-3 pl-3 pr-3">
+		                <?php 
+						//breadcrumbs
+						include($_SERVER['DOCUMENT_ROOT'].'/inc/breadcrumbs.php');
+						?>
+                	</div>
+                    <img src="/assets/img/wild-2.png" class="img-fluid" alt="img">
+                </div>
+                <div>
+                    <h5 class="font-weight-bold">Land on which plantations of trees or shrubs (native or exotic species) have been established for production, or environmental and resource protection purposes. This includes farm forestry. </h5>
+					<div class="pt-4">
+						<p>At level 1, remains vegetation. At level 3, moves from NTV to CTV. Time period: years to decades.</p>
+					</div>
+                  
+                </div>
+                <div class="pt-5">
+                    <h5 class="font-weight-bold mb-4">Available products over Wales:</h5>
+                    <ul class="list-title">
+                        <li class="list-item">
+                        	<a href="#">National Forest Inventory (NFI) woodland map</a>
+                        </li>
+                        <li class="list-item">
+                        	<a href="#">COPERNICUS High Resolution Forest Type</a>
+                        </li>
+                        <li class="list-item">
+                        	<a href="#">NASA Land Cover Type MCD12Q1 (version 6)</a>
+                        </li>
+                        <li class="list-item">
+                        	<a href="#">ESA CCI Land Cover Map (second phase)</a>
+                        </li>
+                        <li class="list-item">
+                        	<a href="#">ESA CORINE Land Cover</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+  		<!--Faciliting-section-end -->
+
+		<!--wales-tab-section-strat -->
+          <div class="col-xs-3 col-sm-3 col-ms-3 col-lg-3 col-xl-3 col-xxl-3  wales-tab">
+			<section class=" wales-tabs-section text-white">
+                <h5 class="tab-title">Wales Ecosystems</h5>
+                <ul class="nav wales-nav-tabs flex-column mb-3">
+                    <li class="wales-nav-item">	
+                        <a class="wales-nav-link list-item text-dark wales-tab-link" data-toggle="tab" href="#tab-1">Ecosystem 1</a>
+                     </li>
+                     <li class="wales-nav-item">
+                        <a class="wales-nav-link list-item active show text-dark wales-tab-link" data-toggle="tab" href="#tab-2">Ecosystem 2</a>
+                     </li>
+                     <li class="wales-nav-item">
+                        <a class="wales-nav-link list-item text-dark wales-tab-link" data-toggle="tab" href="#tab-3">Ecosystem 3</a>
+                     </li>
+                     <li class="wales-nav-item">
+                        <a class="wales-nav-link list-item text-dark wales-tab-link" data-toggle="tab" href="#tab-4">Ecosystem 4</a>
+                     </li>
+                     <li class="wales-nav-item">
+                        <a class="wales-nav-link list-item text-dark wales-tab-link" data-toggle="tab" href="#tab-5">Ecosystem 5</a>
+                     </li>
+                     <li class="wales-nav-item">
+                        <a class="wales-nav-link list-item text-dark wales-tab-link" data-toggle="tab" href="#tab-6">Ecosystem 6</a>
+                     </li>
+                </ul>
+            </section>
+          </div>
+		  		<!--wales-tab-section-end -->
+		</div>
+	</div>
+   <!--Faciliting-section-end -->
+
+<!-- other-wales-ecosystems-start -->
+<div class="container mt-80 mb-80 countrypage-content">
+	<h2 class="common-title">other wales ecosystems <a href="#">View all</a></h2>
+	<div class="row">
+		<ul class="ecosystem-Main">
+			<li class="eco-blocks">
+				<a href="#"><img src="/assets/img/eco-1.png" alt="img" /></a>
+				<div class="data-heading">
+					<h4><a href="#">Ecosystem 1</a></h4>
+				</div>
+			</li>
+			<li class="eco-blocks">
+				<a href="#"><img src="/assets/img/eco-2.png" alt="img" /></a>
+				<div class="data-heading">
+					<h4><a href="#">Ecosystem 2</a></h4>
+				</div>
+			</li>
+			<li class="eco-blocks">
+				<a href="#"><img src="/assets/img/eco-3.png" alt="img" /></a>
+				<div class="data-heading">
+					<h4><a href="#">Ecosystem 3</a></h4>
+				</div>
+			</li>
+			<li class="eco-blocks">
+				<a href="#"><img src="/assets/img/eco-4.png" alt="img" /></a>
+				<div class="data-heading">
+					<h4><a href="#">Ecosystem 4</a></h4>
+				</div>
+			</li>
+			<li class="eco-blocks">
+				<a href="#"><img src="/assets/img/eco-5.png" alt="img" /></a>
+				<div class="data-heading">
+					<h4><a href="#">Ecosystem 5</a></h4>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+	<!-- other-wales-ecosystems-end -->
+
+    <!-- get-in-section-Start -->
+	<div class="container mb-100">		
+		<div class="get-in-section-main">
+			<div class="get-in-section-dsc">
+				<h3>WE NEED YOUR HELP&hellip;</h3>
+				<p>Find out how you contribute to the website by joining our community and helping the Living Earth Hub grow.</p>
+			</div>
+			<button type="button">Get in touch</button>			
+		</div>
+	</div>		
+	<!-- get-in-section-End -->	
+
---
+++ oceania/australia/themes/changes/agriculture-changes/index.php
@@ -0,0 +1,143 @@
+<?php
+// themes/changes/agriculture-changes/
+$activepage=""; include($_SERVER['DOCUMENT_ROOT'].'/header.php'); ?>
+	<!-- country-subpage-banner-section-start -->
+	<div class="row country_subpage-main-section mb-80 mx-0">
+        <div class="col-md-5 country_subpage-background-color m-0 p-0">
+            <div class="mx-md-5 d-flex flex-column h-100 main-content">
+            </div>
+        </div>
+        <div class="col-md-7 m-0 p-0 position-relative">
+            <div class="country_subpage-img-layer"></div>
+            <img class="country_subpage-benner-img" src="/assets/img/wild.png" alt="">
+        </div>
+        <div class="dsc-about-page container">
+			<?php 
+			//breadcrumbs
+			include($_SERVER['DOCUMENT_ROOT'].'/inc/breadcrumbs.php');
+			?>
+            <div class="row">
+	            <div class="dsc-about-inner col-12 col-md-5">
+	                <h3 class="mb-3 mb-md-4 text-uppercase">Wales ECosystems</h3>
+	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+	            </div>       	
+            </div>	
+			<div class="country-subpage-welcome-living-button d-flex justify-content-xs-center justify-content-md-start mt-3">
+				<button type="button" class="country-subpage-search">Lorem Ipsum</button>
+				<button type="button" class="country-subpage-get-in-touch">Lorem Ipsum</button>
+			</div>       
+        </div>
+    </div>
+	<!-- country-subpage-banner-section-end -->
+
+	<!-- country-subpage-blog-start -->
+	<div class="container mt-80 mb-80 future-landscapes-main">
+		<div class="row">
+			<div class="col-12 col-sm-12 col-md-10 offset-md-1 mb-80">
+				<p>Agricultural changes are associated with croplands (seeding, growth, harvesting, ploughing or irrigating) or use (cutting, grazing, fertilising).</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/ploughing/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">PLOUGHING</div>
+					<p>The breaking of ground with into furrows for planting, with this undertaken using a plough</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/ploughing/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/crop-rotation/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">CROP ROTATION</div>
+					<p>Alternating between types of crops on a seasonal basis on the same ground to limit soil degradation</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/crop-rotation/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/land-abandonment/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">LAND ABANDONMENT</div>
+					<p>Land where a previous pattern of agriculture may be observed but which is not currently under production.</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/land-abandonment/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/irrigated-land-abandonment/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">IRRIGATED LAND ABANDONMENT</div>
+					<p>Land where a previous pattern of irrigated agriculture may be observed but which is not currently under production.</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/irrigated-land-abandonment/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/grazing/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">GRAZING</div>
+					<p>A method of animal husbandry whereby domestic livestock are used to convert grass and other forage into meat, milk, wool and other products</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/grazing/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/overgrazing/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">OVERGRAZING</div>
+					<p>Grazing by livestock is used to convert grassland to land used for meat, dairy, or other animal products.</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/overgrazing/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/tillage/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">TILLAGE</div>
+					<p>The preparation of soil for agricultural use through mechanical intervention such as digging, stirring, and overturning.</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/tillage/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/agricultural-expansion/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">AGRICULTURAL EXPANSION</div>
+					<p>The growth of arable lands and pasture used for agricultural food production.</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/agricultural-expansion/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/pasture-degradation/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">PASTURE DEGRADATION</div>
+					<p>The inability of pasture to grow on agricultural land due to overgrazing, degradation, or other poor land management practices.</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/pasture-degradation/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/plantation-forests/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">PLANTATION FORESTS</div>
+					<p>Land on which plantations of trees or shrubs (native or exotic species) have been established for production, or environmental and resource protection purposes. </p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/plantation-forests/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-4">
+				<a href="/themes/changes/agriculture-changes/"><img src="/assets/img/linking-with-farming.png" alt="img"></a>
+				<div class="future-dsc">
+					<div class="future-dsc-title">FALLOW LAND</div>
+					<p>The cultivated land that is not seeded for one or more growing seasons. The maximum idle period is usually less than five years.</p>
+					<p class="pt-2"><a href="/themes/changes/agriculture-changes/" class="learn-more-link">Read more</a></p>
+				</div>
+			</div>								
+		</div>		
+	</div>
+	<!-- country-subpage-blog-end -->
+		
+	<!-- get-in-section-Start -->
+	<div class="container mb-100">		
+		<div class="get-in-section-main">
+			<div class="get-in-section-dsc">
+				<h3>WE NEED YOUR HELP&hellip;</h3>
+				<p>Find out how you contribute to the website by joining our community and helping the Living Earth Hub grow.</p>
+			</div>
+			<button type="button">Get in touch</button>			
+		</div>
+	</div>		
+	<!-- get-in-section-End -->	
